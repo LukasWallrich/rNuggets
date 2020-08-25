f817e0e834268bb08cd9ba12dc60868256518789
Lukas Wallrich <lukas.wallrich@gmail.com>
2020-08-25 22:31:50 +0100

    Include tidy.mira and glance.mira into this package (not in current broom version)

--- R/lm_tables.R
+++ R/lm_tables.R
@@ -1,4 +1,3 @@
-
 #' Creates a summary table comparing standardised and non-standardised linear models
 #'
 #' This function creates a summary table for lm models (including mice::mira objects
@@ -20,8 +19,7 @@
 lm_with_std <- function(mod, std_mod, conf_level = .95, fmt = "%.2f", statistic_vertical = FALSE, filename = NULL, model_names = NULL, show_nimp = FALSE, notes = list(NULL), ...) {
   .check_req_packages(c("modelsummary", "gt", "htmltools", "readr"))
 
-  tidy.mira <- getFromNamespace("tidy.mira", "modelsummary")
-  glance.mira <- getFromNamespace("glance.mira", "modelsummary")
+
 
   if ((class(mod)[1]=="list" | class(std_mod)[1]=="list") & !(length(mod) == length(std_mod))) {
     stop("Same number of models need to be included in mod and std_mod arguments")
@@ -382,3 +380,63 @@ polr_with_std <- function(mod, std_mod, OR = TRUE, conf_level = .95, fmt = "%.2f
   }
 }
 
+#' Tidy  multiple imputation models created with `mice`
+#'
+#' @param x A `mira` object containing multiple models based on `mice` imputations.
+#' @param conf.int Logical. Should confidence intervals be returned. Defaults to true.
+#' @param conf.level Confidence level for intervals. Defaults to .95
+#' @param ... extra arguments (not used)
+#' @note
+#' Available stats in result:
+#' \itemize{
+#'      \item estimate
+#'      \item ubar
+#'      \item b
+#'      \item t
+#'      \item dfcom
+#'      \item df
+#'      \item riv
+#'      \item lambda
+#'      \item fmi
+#'      \item p.value
+#'      \item conf.low (if called with conf.int = TRUE)
+#'      \item conf.high (if called with conf.int = TRUE)
+#' }
+#'
+tidy.mira <- function(x, conf.int = TRUE, conf.level = .95, ...) {
+  out <- summary(mice::pool(x, ...), type = "all", conf.int = conf.int, conf.level = conf.level) %>%
+    dplyr::mutate(term = as.character(term)) %>%
+    tibble::as_tibble()
+  conf_vars <- names(out)[stringr::str_detect(names(out), "%")]
+  names(out)[names(out) %in% conf_vars] <- c("conf.low", "conf.high")
+  out <- out %>% dplyr::select(term, order(names(.)))
+  return(out)
+}
+
+#' Glance a multiple imputation `mice` pooled object
+#'
+#' @param x An object with multiply-imputed models from `mice` (class: `mira`)
+#' @param ... extra arguments (not used)
+#' @return a tibble with one row
+#'
+#' @note If x contains `lm` models, R2 is included in the output
+#'
+#' @examples
+#' library(mice)
+#' data <- airquality
+#' data[4:10,3] <- rep(NA,7)
+#' data[1:5,4] <- NA
+#' tmp <- mice(data,m=5, seed=500, printFlag = FALSE)
+#' mod <- with(tmp, lm(Ozone ~ Solar.R + Wind))
+#' glance(mod)
+#'
+
+glance.mira <- function(x, ...) {
+  out <- tibble::tibble('nimp' = length(x$analyses))
+  out$nobs <- tryCatch(stats::nobs(x$analyses[[1]]), error = function(e) NULL)
+  if (class(x$analyses[[1]])[1] == "lm") {
+    out$r.squared <- mice::pool.r.squared(x, adjusted = FALSE)[1]
+    out$adj.r.squared <- mice::pool.r.squared(x, adjusted = TRUE)[1]
+  }
+  return(out)
+}
