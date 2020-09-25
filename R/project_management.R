3255b3846f673844d51936b0640a7d5fb61db37f
Lukas Wallrich <lukas.wallrich@gmail.com>
2020-09-25 12:50:03 +0100

    Add working setup_analysis_project function

--- R/project_management.R
+++ R/project_management.R
@@ -9,12 +9,14 @@
 #' Otherwise, are placed in root folder
 #' @param standard_packages Character vector of packages to be loaded at start of each analysis file.
 #' @param github_packages Character vector of packages to be loaded and installed from Github if needed at start of each analysis file.
+#' @source The structure is based on https://towardsdatascience.com/how-to-keep-your-research-projects-organized-part-1-folder-structure-10bd56034d3a, with some simplifications and additions.
 #' @export
 
 setup_analysis_project <- function (folder = here::here(), analyses = c("data_prep", "analyses", "presentation"), pipeline_name = "outputs", code_folder = FALSE, standard_packages = c("magrittr", "here", "dplyr"), github_packages = NULL)
 {
 
-  folders <- paste0(folder, c("0_data", "1_tools", (if(code_folder) "2_code" else NULL), paste0("3_", pipeline_name)))
+  pipeline_folder <- paste0("3_", pipeline_name)
+  folders <- paste0(folder, "/", c("0_data", "1_tools", (if(code_folder) "2_code" else NULL), pipeline_folder))
 
   purrr::map(folders, function(x) {
   if (!dir.exists(x)) {
@@ -29,7 +31,7 @@ setup_analysis_project <- function (folder = here::here(), analyses = c("data_pr
   for (i in seq_along(analyses)) {
     filename <- analyses[i]
     previous_name <- paste0(analyses[i-1],"")
-    code <- glue::glue(code_template, .open = "{{", .close = "}}")
+    code <- glue::glue(code_template)
     writeLines(code, file.path(folder, files[i]))
   }
 
@@ -56,23 +58,23 @@ NAME <- "{{filename}}"
 # ------------
 
 if (!require("pacman")) install.packages("pacman")
-pacman::p_load(char = {standard_packages})
-{if(is.null(github_packages)) "" else "pacman::p_load_gh(char = "}
-{if(is.null(github_packages)) "" else github_packages}
+pacman::p_load(char = {paste(standard_packages, collapse=", ")})
+{if(is.null(github_packages)) "" else "pacman::p_load_gh(char = "} \\
+{if(is.null(github_packages)) "" else paste(github_packages, collapse=", ")} \\
 {if(is.null(github_packages)) "" else ")"}
-
 
 source(here("1_tools/management_functions.R"))
 
 #Set up pipeline folder if missing
-pipeline <- createPipelineDir(NAME)
+pipeline <- create_pipeline_dir(NAME)
 datadir <- "0_data"
 pipelinedir <- "3_{pipeline_name}"
 
 #df <- read_!!!(here(datadir, "!!!"))
-#df <- read_!!!(here(pipelinedir, {{previous_name}} , "!!!"))
+#df <- read_!!!(here(pipelinedir, {{previous_name}}, "!!!"))
 
 notes <- character()
+notes <- c(notes, "Note created:", timestamp())
 
 
 # ------------
@@ -90,7 +92,7 @@ writeLines(notes, here({pipeline_folder}, "notes.txt"))
                   ')
 
 management_functions_file <- ("
-  createPipelineDir <- function (NAME) {
+  create_pipeline_dir <- function (NAME) {
   pipeline <- here('3_pipeline', NAME)
   if (!dir.exists(pipeline)) {
     dir.create(pipeline)
